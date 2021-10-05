34e8f961c556525d0adafd3863d65f46dd35f352
ramen08 <90885126+ramen08@users.noreply.github.com>
2021-10-05 10:59:39 +0800

    latest article section, why choose easybank resposnive - 30 mins

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,5 +1,22 @@
 jQuery(function(){
 
     //functions
-    
+    initMatchHeight();
 });
+
+function initMatchHeight(){
+    var $opt1 = {
+        byRow: false,
+        property: 'height',
+        target: null,
+        remove: false
+    };
+
+    var $opt2 = {
+        byRow: true,
+        property: 'height',
+        target: null,
+        remove: false
+    };
+    jQuery('.latest-articles-items .latest-articles-item img').matchHeight($opt2);
+}
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -422,5 +422,22 @@ this},r._applyDataApi=function(){var e={};t("[data-match-height], [data-mh]").ea
 jQuery(function(){
 
     //functions
-    
+    initMatchHeight();
 });
+
+function initMatchHeight(){
+    var $opt1 = {
+        byRow: false,
+        property: 'height',
+        target: null,
+        remove: false
+    };
+
+    var $opt2 = {
+        byRow: true,
+        property: 'height',
+        target: null,
+        remove: false
+    };
+    jQuery('.latest-articles-items .latest-articles-item img').matchHeight($opt2);
+}
